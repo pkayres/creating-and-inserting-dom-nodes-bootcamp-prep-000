1a9ace0615255fa64e61384c15463a6721c95dd4
Pete A <peter.ayresmusic@gmail.com>
2019-01-08 19:54:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,26 +1,19 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
-var element = document.createElement('div')
+var element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-"#f9f9f9"
-<div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, DOM!​</div>​
 document.body.appendChild(element);
-<div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, DOM!​</div>​
 element.style.textAlign = 'center';
-"center"
 var ul = document.createElement('ul');
-
+ 
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li);
 }
-
+ 
 element.appendChild(ul);
-<ul>​…​</ul>​
 ul.style.textAlign = 'left';
-"left"
 ul.removeChild(ul.querySelector('li:nth-child(2)'));
-<li>​2​</li>​
 ul.remove();
